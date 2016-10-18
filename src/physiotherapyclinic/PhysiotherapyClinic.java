724a2143e0d34e1c3f0a321bde499a5b561a61be
guibury <guilherme.bury@gmail.com>
2016-10-18 11:47:08 -0200

    Update main test

--- src/physiotherapyclinic/PhysiotherapyClinic.java
+++ src/physiotherapyclinic/PhysiotherapyClinic.java
@@ -4,7 +4,7 @@ package physiotherapyclinic;
 public class PhysiotherapyClinic {
 
     public static void main(String[] args) {
-    	System.out.println("Testando git");
+    	System.out.println("Testando egit");
     }
     
 }
